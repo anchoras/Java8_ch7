f3daab7948d055d02120459953c0a4bbe3cab202
anchoras <anchoras2@gmail.com>
2021-02-24 19:35:54 +0300

    modified demo for using overriding method in subclasses and for using link variable of superclass for calling subclasses exemplares

--- InheritanceDemo.java
+++ InheritanceDemo.java
@@ -5,18 +5,28 @@
 class TwoDShape {
     private double width;
     private double height;
+    private String name;
 
     TwoDShape() {                   // default constructor
         width = height = 0.0;
+        name = "none";
     }
 
-    TwoDShape(double w, double h) {
+    TwoDShape(double w, double h, String n) {
         width = w;
         height = h;
+        name = n;
     }
 
-    TwoDShape(double x) {           // constructor for figure with even sides
+    TwoDShape(double x, String n) {           // constructor for figure with even sides
         width = height = x;
+        name = n;
+    }
+
+    TwoDShape(TwoDShape obj) {
+        width = obj.width;
+        height = obj.height;
+        name = obj.name;
     }
 
     double getWidth() {
@@ -25,6 +35,9 @@ class TwoDShape {
     double getHeight() {
         return height;
     }
+    String getName() {
+        return name;
+    }
 
     void setWidth(double w) {
         width = w;
@@ -32,9 +45,20 @@ class TwoDShape {
     void setHeight(double h) {
         height = h;
     }
+    void setName(String n) {
+        name = n;
+    }
 
     void showDim() {
         System.out.println("Width: " + width + "   Heigth: " + height);
+    }
+    void showName() {
+        System.out.println("The figure's name is " + name);
+    }
+
+    double area() {
+        System.out.println("This method should be overrided in subclasses");
+        return 0.0;
     }
 }
 
@@ -47,13 +71,18 @@ class Triangle extends TwoDShape {
     }
 
     Triangle (String s, double w, double h) {
-        super(w, h);
+        super(w, h, "Triangle");
         style = s;
     }
 
     Triangle (double x) {
-        super(x);
+        super(x, "Triangle");
         style = "Painted over";
+    }
+
+    Triangle (Triangle obj) {
+        super(obj);
+        style = obj.style;
     }
 
     double area() {
@@ -65,29 +94,60 @@ class Triangle extends TwoDShape {
     }
 }
 
+class Rectangle extends TwoDShape {
+    private boolean isSquare;
+
+    Rectangle() {
+        super();
+        isSquare = false;
+    }
+
+    Rectangle(double w, double h) {
+        super(w, h, "Rectangle");
+        if (w == h)
+            isSquare = true;
+        else
+            isSquare = false;
+    }
+
+    Rectangle(double x) {
+        super(x, "Rectangle");
+        isSquare = true;
+    }
+
+    Rectangle(Rectangle obj) {
+        super(obj);
+        isSquare = obj.isSquare;
+    }
+
+    double area() {
+        return getWidth() * getHeight();
+    }
+
+    void showIsSquare() {
+        if (isSquare)
+            System.out.println("This rectangle is square");
+        else
+            System.out.println("This rectangle is NOT square");
+    }
+
+}
+
 class InheritanceDemo {
     public static void main(String args[]) {
-        Triangle t1 = new Triangle();
-        Triangle t2 = new Triangle("Contour", 8.0, 12.0);
-        Triangle t3 = new Triangle(4.5);
+        TwoDShape[] shapes = new TwoDShape[6];
 
-        System.out.println("Information about t1: ");
-        t1.showDim();
-        t1.showStyle();
-        System.out.println("Area is: " + t1.area());
+        shapes[0] = new Triangle("contour", 8.0, 12.0);
+        shapes[1] = new Rectangle(10);
+        shapes[2] = new Rectangle(10, 4);
+        shapes[3] = new Triangle(7.0);
+        shapes[4] = new TwoDShape(10, 20, "some figure");
+        shapes[5] = shapes[0];
 
-        System.out.println();
-
-        System.out.println("Information about t2: ");
-        t2.showDim();
-        t2.showStyle();
-        System.out.println("Area is: " + t2.area());
-
-        System.out.println();
-
-        System.out.println("Information about t3: ");
-        t3.showDim();
-        t3.showStyle();
-        System.out.println("Area is: " + t3.area());
+        for(int i=0; i<shapes.length; i++) {
+            System.out.println("Object -- " + shapes[i].getName());
+            System.out.println("Area is: " + shapes[i].area());
+            System.out.println();
+        }
     }
 }
